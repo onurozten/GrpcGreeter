9e17263f08c714d1a7df249126e0111af7d248d7
OnuR <onur301@gmail.com>
2023-09-01 13:34:57 +0300

    Add project files.

---
+++ src/GrpcGreeter/GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GreeterClient;
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+
+// The port number must match the port of the gRPC server.
+//using var channel = GrpcChannel.ForAddress("https://localhost:7185");
+using var channel = GrpcChannel.ForAddress("http://localhost:5187");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
